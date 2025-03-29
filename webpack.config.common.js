649182154b2a94e05c257a7501b42730061b510a
Alexandr Rusin <i@arusin.ru>
2025-03-29 13:59:36 +0700

    feat: добавил copyWebPack плагин для статических файлов

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -15,6 +16,14 @@ module.exports = {
       template: path.resolve(__dirname, "public/index.html"),
     }),
     new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
