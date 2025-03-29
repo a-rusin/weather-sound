33afdc4c9031c3bf67123d0e2a6cbda3e367e964
Alexandr Rusin <i@arusin.ru>
2025-03-29 15:24:59 +0700

    feat: добавил возможность включать музыку по клику на картинку

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./css/style.scss";
 
-console.log("test1!");
+document.addEventListener("DOMContentLoaded", () => {
+  const soundItems = document.querySelectorAll(".sound__item");
+  const listItems = document.querySelectorAll(".list__item");
+
+  listItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      const itemAtr = item.getAttribute("data-list-item");
+      if (itemAtr) {
+        playSound(itemAtr);
+      }
+    });
+  });
+
+  function playSound(atr) {
+    stopAllSounds();
+    const music = document.querySelector(`.sound__item_${atr}`);
+    music.play();
+  }
+
+  function stopAllSounds() {
+    soundItems.forEach((audio) => {
+      console.log(audio);
+      audio.pause();
+      audio.currentTime = 0;
+    });
+  }
+});
