05abf2a14e97491d722cf004f3e2252f1829e9cd
Alexandr Rusin <i@arusin.ru>
2025-03-29 18:12:33 +0700

    feat: добавил возможность смены фоновой картинки

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import "./css/style.scss";
 document.addEventListener("DOMContentLoaded", () => {
   const soundItems = document.querySelectorAll(".sound__item");
   const listItems = document.querySelectorAll(".list__item");
+  const bgItems = document.querySelectorAll(".bg-content__item");
 
   listItems.forEach((item) => {
     item.addEventListener("click", () => {
       const itemAtr = item.getAttribute("data-list-item");
       if (itemAtr) {
         playSound(itemAtr);
+        changeBG(itemAtr);
       }
     });
   });
@@ -17,6 +19,18 @@ document.addEventListener("DOMContentLoaded", () => {
     stopAllSounds();
     const music = document.querySelector(`.sound__item_${atr}`);
     music.play();
+  }
+
+  function changeBG(atr) {
+    const activeBg = document.querySelector(`.bg-content__item_${atr}`);
+
+    bgItems.forEach((bg) => {
+      if (bg !== activeBg) {
+        bg.classList.remove("active");
+      } else {
+        bg.classList.add("active");
+      }
+    });
   }
 
   function stopAllSounds() {
